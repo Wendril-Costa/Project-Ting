0be00e98836be9cc69776ef067be62e172f04961
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-26 00:39:29 -0300

    Requisito 7 Feito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for file in map(instance.search, range(len(instance))):
+        occurrences = []
+        for index, line in enumerate(file['linhas_do_arquivo'], start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index})
+        if occurrences:
+            result = {
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            }
+            results.append(result)
+    return results
 
 
 def search_by_word(word, instance):
