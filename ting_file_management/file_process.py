354617f76531570f066dc6b18a894d93eb1d7aa5
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-25 23:20:22 -0300

    Requisito 4 Feito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,7 +3,6 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    # Verifica se o arquivo já foi processado anteriormente
     if path_file in [item["nome_do_arquivo"] for item in instance.items]:
         print(
             f"Arquivo {path_file} já foi processado anteriormente",
@@ -11,25 +10,32 @@ def process(path_file, instance):
         )
         return
 
-    # Importa o conteúdo do arquivo usando a função txt_importer
     lines = txt_importer(path_file)
 
-    # Cria o dicionário com as informações do arquivo
     file_info = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(lines),
         "linhas_do_arquivo": lines,
     }
 
-    # Adiciona o dicionário à fila
     instance.enqueue(file_info)
 
-    # Mostra os dados processados via stdout
     print(file_info, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return
+
+    try:
+        removed_file = instance.dequeue()
+        print(
+            f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso",
+            file=sys.stdout,
+        )
+    except IndexError:
+        print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
