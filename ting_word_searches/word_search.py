e3f7df5be81ebb469f64f8b7291a21135ab6ccf2
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-26 00:50:32 -0300

    Requisito 8 Feito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,19 +1,34 @@
+def find_occurrences(word, lines):
+    return [
+        {"linha": index + 1}
+        for index, line in enumerate(lines)
+        if word.lower() in line.lower()
+    ]
+
+
 def exists_word(word, instance):
-    results = []
-    for file in map(instance.search, range(len(instance))):
-        occurrences = []
-        for index, line in enumerate(file['linhas_do_arquivo'], start=1):
-            if word.lower() in line.lower():
-                occurrences.append({"linha": index})
-        if occurrences:
-            result = {
-                "palavra": word,
-                "arquivo": file['nome_do_arquivo'],
-                "ocorrencias": occurrences
-            }
-            results.append(result)
-    return results
+    return [
+        {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": find_occurrences(word, file["linhas_do_arquivo"]),
+        }
+        for file in map(instance.search, range(len(instance)))
+        if find_occurrences(word, file["linhas_do_arquivo"])
+    ]
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return [
+        {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1, "conteudo": line}
+                for index, line in enumerate(file["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }
+        for file in map(instance.search, range(len(instance)))
+        if find_occurrences(word, file["linhas_do_arquivo"])
+    ]
