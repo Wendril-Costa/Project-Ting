2923a08593c136c95d5c6d0017906c4b30b9a6ce
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-26 00:02:54 -0300

    Requisito 6 Feito

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,20 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    mock_data = [
+        {"nome_do_arquivo": "file1.txt", "qtd_linhas": 3},
+        {"nome_do_arquivo": "file2.txt", "qtd_linhas": 6},
+        {"nome_do_arquivo": "file3.txt", "qtd_linhas": 2},
+    ]
+    [pq.enqueue(item) for item in mock_data]
+    assert len(pq) == 3
+    removed_file = pq.dequeue()
+    assert removed_file is not None
+    assert removed_file["nome_do_arquivo"] == "file1.txt"
+    assert pq.search(0)["nome_do_arquivo"] == "file3.txt"
+    assert len(pq) == 2
+    with pytest.raises(IndexError):
+        pq.search(len(pq))
